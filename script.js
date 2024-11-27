d44258efd3b1708fca6f42fc9605018fbfce1414
Antony Str <155707235+Rafaelalon0509@users.noreply.github.com>
2024-11-27 12:26:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function fetchData() {
 
     resultDiv.textContent = "Cargando datos...";
 
-    // Obtenemos el número del usuario ingresado (puede venir de un input)
+   
     const userId = parseInt(document.getElementById("userIdInput").value);
 
     if (isNaN(userId) || userId < 1 || userId > 10) {
@@ -22,11 +22,11 @@ function fetchData() {
             return response.json(); // Convertir la respuesta a JSON
         })
         .then(data => {
-            // Filtramos el usuario con el ID proporcionado
+            
             const user = data.find(u => u.id === userId);
 
             if (user) {
-                // Construimos un objeto con los campos requeridos
+                
                 const filteredData = {
                     nombre: user.name,
                     email: user.email,
@@ -35,7 +35,6 @@ function fetchData() {
                     compañia: user.company.name
                 };
 
-                // Mostrar los detalles del usuario filtrado
                 resultDiv.textContent = JSON.stringify(filteredData, null, 2);
             } else {
                 resultDiv.textContent = "Usuario no encontrado.";
